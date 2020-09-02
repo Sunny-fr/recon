9072f0126daaf1c4bccfeca99c4bd446293367c4
Alaa El Helw <sicksadsun@gmail.com>
2020-09-02 10:34:19 +0200

    remove unused

--- redo.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-docker stop recon
-docker rm recon
-docker build --build-arg build_front="true" -t sunny/recon .
--- run.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-docker run -p 49160:8080 -d  --name recon -v /Users/sunny/workspace/recon/server/share/output:/usr/src/app/share/output sunny/recon:latest
