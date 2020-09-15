660beb14cfab74e5f05486aafa23c603bd821fed
Alaa El Helw <sicksadsun@gmail.com>
2020-09-15 11:26:35 +0200

    Fix Download filename

--- server/src/modules/download/controller.js
+++ server/src/modules/download/controller.js
@@ -5,6 +5,7 @@ const download = (req, res) => {
     const files = getDatabase('history')
 
     const file = files.find(p => p.id === id)
+
     if(!file) {
         return res.status('404').json({
             message: 'file not found'
@@ -16,9 +17,13 @@ const download = (req, res) => {
         })
     }
     const filename = file.data.name
-        const extension = filename.split('.').pop()
+    //const extension = filename.split('.').pop()
+    //const outputFileName = filename.replace('.' + extension, '.mp4')
+    //const outputFileName = file.data.name //filename.replace('.' + extension, '.mp4')
 
-    return res.download(file.data.output, filename.replace('.' + extension, '.mp4') );
+
+
+    return res.download(file.data.output, filename );
 }
 
 module.exports = {
